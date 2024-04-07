7859726a88514e897a4374716355ec807d5711ac
Samuel-Arteaga <samuel.arteaga.7e6@itb.cat>
2024-04-07 15:58:33 +0200

    Update camera 1er person

--- Survival-M17UF4/Assets/Scripts/InputManager.cs
+++ Survival-M17UF4/Assets/Scripts/InputManager.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+    public bool Apuntar;
+    private InputAction apuntarAction;
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        apuntarAction = playerInput.actions.FindAction("Aim");
+    }
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+    //1er Persona
+    public void OnApuntar(InputValue value)
+    {
+        ApuntarInput(value.isPressed);
+    }
+    public void ApuntarInput(bool newAimState)
+    {
+        Apuntar = newAimState;
     }
 
     // Update is called once per frame
