06d1c55ecfc4daa73f9780d98e1a35cefbae320b
Samuel-Arteaga <samuel.arteaga.7e6@itb.cat>
2024-04-20 21:28:26 +0200

    Update Barra de vida Enemigo

--- Survival/Assets/Scripts/IA/EnemyController.cs
+++ Survival/Assets/Scripts/IA/EnemyController.cs
@@ -1,39 +1,52 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyController : StateController
 {
     public float AttackDistance;
     public float HP;
-    private float nextHurt = 0;
+    private float maxHP = 100f; 
     private Animator anim;
+    public Image healthBar;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
     }
+
     private void Update()
     {
         StateTransition();
         if (currentState.action != null) currentState.action.OnUpdate();
-        if(Input.GetKey(KeyCode.J) && Time.time >= nextHurt)
+
+        
+        if (Input.GetKeyDown(KeyCode.J))
         {
-            OnHurt(1);
-            nextHurt = Time.time + 0.3f;
+            OnHurt(10); 
+            UpdateHealthBarInstant(); 
         }
     }
 
     public void OnHurt(float damage)
     {
         HP -= damage;
+        HP = Mathf.Clamp(HP, 0, maxHP);
+    }
+
+    private void UpdateHealthBarInstant()
+    {
+        
+        healthBar.fillAmount = HP / maxHP;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player")) target = other.gameObject;
+        if (other.CompareTag("Player")) target = other.gameObject;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.CompareTag("Player")) target = null;
+        if (other.CompareTag("Player")) target = null;
     }
 }
+
